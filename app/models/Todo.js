7e0679eb87b14ff0b94275e3f6788905d90e546f
LanceFontanilla <lancefontanilla@gmail.com>
2023-09-02 00:50:27 -0600

    todo complete, next is todo form

--- app/models/Todo.js
+++ app/models/Todo.js
@@ -14,7 +14,14 @@ export class Todo {
 
     get TodoTemplate() {
         return `
- 
+        <div class="container-fluid">
+            <div class="row justify-content-evenly d-flex align-items-center">
+                <div class="col-1 form-check text-dark">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                </div>
+            <span class=" col-8 align-items-center">${this.description}</span>
+            <button class="col-1 btn btn-dark text-light justify-content-center d-flex"><span class="mdi mdi-delete-forever"> </span></button>
+        </div>
         
         `
     }
